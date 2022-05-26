47706a53255886e8b5be0659b319007084023d8d
Nomango <nomango@qq.com>
2022-05-26 15:30:41 +0800

    缩短等待时间

--- src/Home.jsx
+++ src/Home.jsx
@@ -63,10 +63,10 @@ function Home() {
         setShowArrowDown(false);
       }
     } else {
-      // 等待5秒后必须进入页面
+      // 等待4秒后必须进入页面
       setTimeout(() => {
         setLoading(false);
-      }, 5000);
+      }, 4000);
     }
     return () => { mounted.current = false; };
   }, []);
